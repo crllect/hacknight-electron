a5b177664eb8326240f76c23c1544afd4f42a575
Crllect <144494583+crllect@users.noreply.github.com>
2024-06-02 01:29:10 -0400

    stuff

--- main.js
+++ main.js
@@ -2,30 +2,30 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 const { SerialPort } = require('serialport');
 
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            enableRemoteModule: true
-        }
-    });
+	const win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+			enableRemoteModule: true
+		}
+	});
 
-    win.loadFile('index.html');
+	win.loadFile('index.html');
 
-    const port = new SerialPort('COM3', {
-        baudRate: 9600
-    });
+	const port = new SerialPort('COM3', {
+		baudRate: 9600
+	});
 
-    port.on('data', function (data) {
-        console.log('Data:', data);
-        win.webContents.send('data-received');
-    });
+	port.on('data', function (data) {
+		console.log('Data:', data);
+		win.webContents.send('data-received');
+	});
 
-    ipcMain.on('submit-time', (event, time) => {
-        port.write(time);
-    });
+	ipcMain.on('submit-time', (event, time) => {
+		port.write(time + 'e');
+	});
 }
 
 app.whenReady().then(createWindow);
