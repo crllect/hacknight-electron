e79c001635620665f7e89a9a80cbae3637b9b5da
Crllect <144494583+crllect@users.noreply.github.com>
2024-06-02 06:44:08 -0400

    gssgsgdsgd

--- renderer.js
+++ renderer.js
@@ -6,45 +6,59 @@ const { SerialPort } = require('serialport');
 const tableify = require('tableify');
 
 async function listSerialPorts() {
-    await SerialPort.list().then((ports, err) => {
-        if (err) {
-            document.getElementById('error').textContent = err.message;
-            return;
-        } else {
-            document.getElementById('error').textContent = '';
-        }
+	await SerialPort.list().then((ports, err) => {
+		if (err) {
+			document.getElementById('error').textContent = err.message;
+			return;
+		} else {
+			document.getElementById('error').textContent = '';
+		}
+		console.log('ports', ports);
+
+		if (ports.length === 0) {
+			document.getElementById('error').textContent =
+				'No ports discovered';
+		}
         console.log('ports', ports);
 
-        if (ports.length === 0) {
+
+		tableHTML = tableify(ports);
+		document.getElementById('ports').innerHTML = tableHTML;
+        document.getElementById('error').textContent =
+				'Weve made it this far';
+		// Find the port with the manufacturer "Adafruit"
+		const adafruitPort = ports.find(
+			port => port.manufacturer === 'Adafruit'
+		);
+        document.getElementById('error').textContent =
+				'Right after adafruit port check';
+		console.log('Adafruit port:', adafruitPort);
+        document.getElementById('error').textContent =
+				adafruitPort;
+		if (adafruitPort) {
             document.getElementById('error').textContent =
-                'No ports discovered';
-        }
-
-        tableHTML = tableify(ports);
-        document.getElementById('ports').innerHTML = tableHTML;
-
-        // Find the port with the manufacturer "Adafruit"
-        const adafruitPort = ports.find(port => port.manufacturer === 'Adafruit');
-        console.log('Adafruit port:', adafruitPort);
-        if (adafruitPort) {
-            console.log('Adafruit port found:', adafruitPort);
-            // Open a new SerialPort instance for the Adafruit port
-            const port = new SerialPort(adafruitPort.path, { baudRate: 9600 });
-
-            // Write data to the port
-            port.write('000100e', (err) => {
-                if (err) {
-                    return console.log('Error on write: ', err.message);
-                }
-                console.log('Message written');
-            });
-        }
-    });
+				'ohhh shittt';
+			console.log('Adafruit port found:', adafruitPort);
+			// Open a new SerialPort instance for the Adafruit port
+			const port = new SerialPort(adafruitPort.path, { baudRate: 9600 });
+            document.getElementById('error').textContent =
+            'naaaa';
+			// Write data to the port
+			port.write('000100e', err => {
+				if (err) {
+					return document.getElementById('error').textContent =
+                    'big error :c ' + err.message;
+				}
+				document.getElementById('error').textContent =
+				'Message Written';
+			});
+		}
+	});
 }
 
 function listPorts() {
-    listSerialPorts();
-    setTimeout(listPorts, 2000);
+	listSerialPorts();
+	setTimeout(listPorts, 2000);
 }
 
 // Set a timeout that will check for new serialPorts every 2 seconds.
@@ -52,7 +66,6 @@ function listPorts() {
 setTimeout(listPorts, 2000);
 
 listSerialPorts();
-
 
 // path: /dev/tty.usbmodem101
 // manufacturer: Adafruit
